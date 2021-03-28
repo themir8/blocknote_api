03c9e612399f8e9a320dcf7aab64c8a2872d98f2
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-28 10:38:07 +0500

    Added views for GroupArticles

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,40 +1,29 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
-from django.contrib.auth import login
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import UpdateAPIView
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import UpdateAPIView, GenericAPIView
 from rest_framework.mixins import UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from .models import Article, GroupArticles
-from .serializers import (
-    ArticleListSerializer,
-    ArticleDetailSerializer,
-    ArticleCreateSerializer
-    )
+from . import serializers
+from .permissions import IsAuthenticatedAndOwnerOrReadOnly
 from .services import generate_url
 
 
 class ArticleListView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
         articles = Article.objects.all()
-        serializer = ArticleListSerializer(articles, many=True)
-        return Response(serializer.data)
-
-
-class ArticleDetailView(APIView):
-    def get(self, request, pk):
-        article = Article.objects.get(id=pk)
-        serializer = ArticleDetailSerializer(article)
+        serializer = serializers.ArticleListSerializer(articles, many=True)
         return Response(serializer.data)
 
 
 class ArticleCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        article = ArticleCreateSerializer(data=request.data)
+        article = serializers.ArticleCreateSerializer(data=request.data)
         if article.is_valid():
             article.save()
         return Response(status=201)
@@ -42,80 +31,40 @@ class ArticleCreateView(APIView):
 
 class ArticleEditView(GenericAPIView, UpdateModelMixin):
     queryset = Article.objects.all()
-    serializer_class = ArticleCreateSerializer
+    serializer_class = serializers.ArticleCreateSerializer
+    permission_classes = [IsAuthenticatedAndOwnerOrReadOnly]
 
     def get(self, request, pk):
         article = Article.objects.get(id=pk)
-        serializer = ArticleDetailSerializer(article)
+        serializer = serializers.ArticleDetailSerializer(article)
         return Response(serializer.data)
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
 
-# # GroupArticles views
-# class PublicGroupList(LoginRequiredMixin, ListView):
-#     model = GroupArticles
-#     queryset = GroupArticles.objects.filter(private=False).order_by('-created_date')
-#     context_object_name = 'group_list'
-#     template_name = "main/p_group_list.html"
+# GroupArticles views
+class PublicGroupListView(APIView):
+    permission_classes = [IsAuthenticatedAndOwnerOrReadOnly]
+
+    def get(self, request):
+        groups = GroupArticles.objects.filter(private=False)
+        serializer = serializers.GroupArticleListSerializer(groups, many=True)
+        return Response(serializer.data)
 
 
-# class GroupList(LoginRequiredMixin, ListView):
-#     model = GroupArticles
-#     context_object_name = 'group_list'
+class GroupListView(APIView):
+    permission_classes = [IsAuthenticatedAndOwnerOrReadOnly]
+
+    def get(self, request):
+        groups = GroupArticles.objects.all()
+        serializer = serializers.GroupArticleListSerializer(groups, many=True)
+        return Response(serializer.data)
 
 
-# class GroupDetail(LoginRequiredMixin, DetailView):
-#     model = GroupArticles
-#     context_object_name = 'group'
-#     slug_url_kwarg = 'slug'
-#     query_pk_and_slug = True
-# # end
-
-
-
-# class CustomLoginView(LoginView):
-#     template_name = 'main/login.html'
-#     fields = '__all__'
-#     redirect_authenticated_user = True
-
-#     def get_success_url(self):
-#         return reverse_lazy('index')
-
-
-# class RegisterPage(FormView):
-#     template_name = 'main/registration.html'
-#     form_class = UserCreationForm
-#     redirect_authenticated_user = True
-#     success_url = reverse_lazy('index')
-
-#     def form_valid(self, form):
-#         user = form.save()
-#         if user is not None:
-#             login(self.request, user)
-#         return super(RegisterPage, self).form_valid(form)
-
-#     def get(self, *args, **kwargs):
-#         if self.request.user.is_authenticated:
-#             return redirect('index')
-#         return super(RegisterPage, self).get(*args, **kwargs)
-
-# def ArticleViewEdit(request, slug):
-#     instance = Article.objects.get(url=slug)
-#     article_form = ArticleEditForm(request.POST or None, instance=instance)
-#     if article_form.is_valid():
-#         instance.title=request.POST.get("title")
-#         instance.body=request.POST.get("body")
-#         instance.save()
-#         return redirect('article', slug=slug)
-#     else:
-#         error = 'Form is invalid'
-#     context = {
-#         'article_form': article_form,
-#         'error': error,
-#         'color': 'success',
-#         'form_status': True,
-#         'article': instance
-#     }
-#     return render(request, 'main/viewedit.html', context)
+class GroupDetailView(APIView):
+    def get(self, request, pk):
+        group = GroupArticles.objects.get(id=pk)
+        serializer = serializers.GroupArticleDetailSerializer(group)
+        return Response(serializer.data)
+# end
