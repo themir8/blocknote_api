33d88d1873539d1f71dc7d44be93acefaa87d310
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-28 10:37:19 +0500

    Added a new serializer classes: for model GroupArticles

--- apps/main/serializers.py
+++ apps/main/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Article
+from .models import Article, GroupArticles
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
@@ -8,17 +8,44 @@ class ArticleListSerializer(serializers.ModelSerializer):
         model = Article
         fields = ['title', 'url']
 
+
 class ArticleDetailSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    
     class Meta:
         model = Article
         fields = '__all__'
+
 
 class ArticleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
 
+
+# GroupArticles views
+class GroupArticleListSerializer(serializers.ModelSerializer):
+    articles = serializers.SlugRelatedField(slug_field="url", many=True, read_only=True)
+    
+    class Meta:
+        model = GroupArticles
+        fields = ['name', 'articles', 'slug']
+
+
+class GroupArticleDetailSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    articles = serializers.SlugRelatedField(slug_field="url", many=True, read_only=True)
+
+    class Meta:
+        model = GroupArticles
+        fields = '__all__'
+
+
+class GroupArticleCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroupArticles
+        fields = '__all__'
+# end
 
 # class GroupArticlesSerializer(serializers.HyperlinkedModelSerializer):
 #     class Meta:
